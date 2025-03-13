86ada2ed8e6a40fa9e33b5d25a5aad6a161e9803
renzi febriandika <renzifebriandika923@gmail.com>
2025-03-14 01:22:15 +0700

    add Zustand store for anime data management and implement home page functionality

---
+++ src/app/(main)/page.tsx
@@ -0,0 +1,16 @@
+"use client";
+
+import { useHomeStore } from "@/store/useHomeStore";
+import { useEffect } from "react";
+
+export default function Home() {
+  const { fetchAnime, completeAnime } = useHomeStore();
+
+  useEffect(() => {
+    fetchAnime();
+  }, []);
+  console.log(completeAnime);
+  return (
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]"></div>
+  );
+}
---
+++ src/store/useHomeStore.ts
@@ -0,0 +1,32 @@
+import { completeAnime, ongoingAnime } from "@/types/response";
+import { create } from "zustand";
+
+interface AnimeState {
+  ongoingAnime: ongoingAnime[];
+  completeAnime: completeAnime[];
+  loading: boolean;
+  error: string | null;
+  fetchAnime: () => Promise<void>;
+}
+
+export const useHomeStore = create<AnimeState>((set) => ({
+  completeAnime: [],
+  ongoingAnime: [],
+  loading: false,
+  error: null,
+  fetchAnime: async () => {
+    set({ loading: true, error: null });
+    try {
+      const response = await fetch(`${process.env.NEXT_PUBLIC_APIURL}/home`);
+      const result = await response.json();
+
+      set({
+        ongoingAnime: result.data.ongoing_anime,
+        completeAnime: result.data.complete_anime,
+        loading: false,
+      });
+    } catch (error) {
+      set({ error: "Failed to fetch anime data", loading: false });
+    }
+  },
+}));
