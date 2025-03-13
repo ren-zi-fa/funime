7df6fd81f477a764a4eac7ac6d860075d4681d74
renzi febriandika <renzifebriandika923@gmail.com>
2025-03-14 02:25:44 +0700

    add OngoingCard component to display ongoing anime and update home page layout

---
+++ src/app/(main)/_components/ongoingCard.tsx
@@ -0,0 +1,53 @@
+"use client";
+
+import { Card } from "@/components/ui/card";
+import { cn } from "@/lib/utils";
+import { useHomeStore } from "@/store/useHomeStore";
+import { ongoingAnime } from "@/types/response";
+import { Star } from "lucide-react";
+import Image from "next/image";
+import Link from "next/link";
+import { useEffect } from "react";
+
+export default function OngoingCard() {
+  const { completeAnime, error, fetchAnime, loading, ongoingAnime } =
+    useHomeStore();
+  useEffect(() => {
+    fetchAnime();
+  }, []);
+  if (loading) return <p>Loading..</p>;
+
+  return (
+    <div className="grid grid-cols-2 p-4 gap-2">
+      {ongoingAnime.map((anime, index) => (
+        <Card
+          className="relative group  shadow-md h-[250px] cursor-pointer"
+          key={index}
+        >
+          <Link href={`/anime/${anime.slug}`}>
+            <Image
+              src={anime.poster || ""}
+              alt={anime.title || "anime"}
+              fill
+              className="object-cover w-full h-full"
+            />
+            <p
+              className={cn(
+                "absolute bottom-0 w-full bg-black/50  mt-10 text-white text-center transition-all duration-200 overflow-hidden",
+                "group-hover:h-7  p-2"
+              )}
+            >
+              {anime.title}
+            </p>
+            <div className="w-full ">
+              <p className="absolute top-0 bottom-0 bg-opacity-75 flex items-center gap-2 h-fit text-white px-2 right-0 mt-16 bg-blue-700  shadow-md">
+                <Star width={15} />
+                <span className="text-sm">{anime.current_episode}</span>
+              </p>
+            </div>
+          </Link>
+        </Card>
+      ))}
+    </div>
+  );
+}
