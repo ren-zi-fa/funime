b34b452a2edecda79434efcc1890a1a0074d4239
renzi febriandika <renzifebriandika923@gmail.com>
2025-03-17 11:37:32 +0700

    feat: update metadata for Funime and remove unused assets

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,6 +1,11 @@
+import { Metadata } from "next";
 import CompletedCard from "./_components/completeCard";
 import OngoingCard from "./_components/ongoingCard";
 
+export const metadata: Metadata = {
+  title: "Funime",
+  description: "made by renzi febriandika",
+};
 export default function Home() {
   return (
     <div className="ms-6 h-full">
